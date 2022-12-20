655bc353ba476e47602beed8cbe51f24d933d807
Sidorik Ilia <iss19.spb@gmail.com>
2022-12-20 18:09:23 +0700

    Initial Commit

---
+++ Dont work Header for tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dont work Header for tableView
+//
+//  Created by Илья Сидорик on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
